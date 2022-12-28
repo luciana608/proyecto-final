3f8d62dcd71c2025b58467a89581c3ffe1ca9554
luciana608 <121557894+luciana608@users.noreply.github.com>
2022-12-28 11:52:42 -0300

    Add files via upload

---
+++ Proyecto Final/app.js
@@ -0,0 +1,20 @@
+let nombre = prompt("¿Cuál es el nombre de la serie?");
+function saludar(nombre) {
+    console.log('La exitosa serie de televisión americana es conocida como Ricky y Morty');
+}
+
+saludar(nombre);
+
+let temporada = prompt("¿Cuántas temporadas tuvo la serie?");
+function saludar(temporada) {
+    console.log('La famosa serie de televisión americana de animación para adultos tuvo en total 6 temporadas');
+}
+
+saludar(6);
+
+let plataforma = prompt("¿ A cuál plataforma me suscribo para ver la serie completa?");
+function saludar(plataforma) {
+    console.log('La plataforma por la cuál podes ver la serie completa es HBO Max');
+}
+
+saludar(plataforma);
